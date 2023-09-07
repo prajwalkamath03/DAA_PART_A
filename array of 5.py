98ec2790c1efa815c8b7370686e6b3e03397804e
Prajwal Kamath <125379835+prajwalkamath03@users.noreply.github.com>
2023-09-07 11:55:08 +0530

    Create array of 5.py

---
+++ array of 5.py
@@ -0,0 +1,7 @@
+# Create an array of 5 integers
+integer_array = [10, 20, 30, 40, 50]
+
+# Display the array items
+print("Array items:")
+for i in range(len(integer_array)):
+    print(f"Element {i}:", integer_array[i])
